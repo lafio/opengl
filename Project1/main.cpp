8cc1f9c441ee4e53a18306d43b0e53e7c4f0ace1
xiao <lichengxiao1995@126.com>
2021-01-18 23:17:13 +0800

    从外部读档； 用自己的txt档编写shader； 画一个四边形

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,7 +2,21 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-using namespace std;
+#include "Shader.h"
+
+void processInput(GLFWwindow* window);
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f, 1.0f, 0, 0, //0
+	 0.5f, -0.5f, 0.0f, 0, 1.0f, 0, //1
+	 0.0f,  0.5f, 0.0f, 0, 0, 1.0f, //2
+	 0.8f, 0.8f, 0.0f, 1.0f, 0, 1.0f //3
+};
+
+unsigned int indices[] = {
+	0,1,2,
+	2,3,1
+};
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -12,7 +26,7 @@ int main() {
 	//open GLFW window
 	GLFWwindow* window = glfwCreateWindow(800,600,"my openGL game",NULL,NULL);
 	if (window == NULL) {
-		cout << "open openGL failed" << endl;
+		std::cout << "open openGL failed" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
@@ -21,18 +35,58 @@ int main() {
 	//Init GLEW
 	glewExperimental = true;
 	if (glewInit() != GLEW_OK) {
-		cout << "failed to init GLEW" << endl;
+		std::cout << "failed to init GLEW" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
 	glViewport(0, 0, 800, 600);
+	Shader* myShader = new Shader("vertexSource.txt", "fragmentSource.txt");
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1,&VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(6, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(6);
+	glVertexAttribPointer(7, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(7);
 
 	while (!glfwWindowShouldClose(window)) {
+		// input
+		processInput(window);
+
+		//rending commands
+		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+		myShader->use();
+		
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//check and call events and setup the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
