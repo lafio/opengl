67f43d5e11b3e237325a5da23e903fbb16d57f6a
xiao <lichengxiao1995@126.com>
2021-01-23 23:04:09 +0800

    coordinate system

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -105,7 +105,12 @@ int main() {
 
 	//calculate our transformation
 	//trans = glm::scale(trans, glm::vec3(2.0f, 2.0f, 2.0f));
-
+	glm::mat4 modelMat;
+	modelMat = glm::rotate(modelMat, glm::radians(-55.0f), glm::vec3(1.0f, 0, 0));
+	glm::mat4 viewMat;
+	viewMat = glm::translate(viewMat, glm::vec3(0, 0, -3.0f));
+	glm::mat4 projMat;
+	projMat = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 	while (!glfwWindowShouldClose(window)) {
 		//trans = glm::translate(trans, glm::vec3(-0.01f, 0, 0));
 		glm::mat4 trans;
@@ -128,7 +133,10 @@ int main() {
 		myShader->use();
 		glUniform1i(glGetUniformLocation(myShader->ID, "ourTexture"), 0);
 		glUniform1i(glGetUniformLocation(myShader->ID, "ourFace"), 1);
-		glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+		//glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+		glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+		glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "viewMat"), 1, GL_FALSE, glm::value_ptr(viewMat));
+		glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "projMat"), 1, GL_FALSE, glm::value_ptr(projMat));
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//check and call events and setup the buffers
 		glfwSwapBuffers(window);
